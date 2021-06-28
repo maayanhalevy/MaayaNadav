507adf6c0e9059d1d4ea5fadc878f74b62c68262
nadavhalahmi <nadavhalahmi560@gmail.com>
2021-06-28 12:08:23 +0300

    added homepage.html and js

---
+++ site/js/index.js
@@ -0,0 +1,14 @@
+// const loginForm = document.getElementById("login-form");
+// const loginErrorMsg = document.getElementById("login-error-msg");
+const loginButton = document.getElementById("login-form-submit");
+
+loginButton.addEventListener("click", (e) => {
+    const username = document.getElementById("username-field").value;
+    const password = document.getElementById("password-field").value;
+    e.preventDefault();
+
+    if (username === "Nadav" && password === "12345") {
+        alert("You have successfully logged in.");
+        window.location.replace("/homepage.html");
+    } 
+})
